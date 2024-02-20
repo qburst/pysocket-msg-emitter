cd1173333401c721964cf73813e73c18a218d641
ajeesh.anil <ajeesh.anil@qburst.com>
2024-02-20 12:27:46 +0530

    multiple namespace and rooms handling in Emitter class

--- redisemitter.py
+++ redisemitter.py
@@ -1,21 +1,29 @@
 import json
 import redis
 import base64
-import logging
 
 
 class Emitter:
     def __init__(
-        self, host="localhost", port=6379, namespace="/", key="socket.io", password=None
+            self,
+            host="localhost",
+            port=6379,
+            namespace=[],
+            key="socket.io",
+            password=None
     ):
 
         self.host = host
         self.port = port
-        self.namespace = namespace
+
+        # Receives namespace to be connected with, if not provided then uses default namespace
+        if namespace:
+            self.namespace = self._flatten_namespaces(namespace)
+        else:
+            self.namespace = ['/']
         self.password = password
         self.key = key
         self.rooms = []
-
         self.redis_client = self._create_client()
 
     def in_room(self, *rooms):
@@ -24,53 +32,70 @@ class Emitter:
         return self
 
     def of_namespace(self, namespace):
-        """Set the namespace."""
-        self.namespace = namespace
+        """Includes the namespace to the namespace list"""
+        self.namespace.append("/"+namespace)
         return self
 
-    def emit(self, event, *args):
+    def off_namespace(self, namespace):
+        """Removes namespace given from the namespace list"""
+        try:
+            self.namespace.remove("/"+namespace)
+        finally:
+            return self
+
+    def emit(self, event, *args, **kwargs):
         """Emit an event with optional arguments to the specified rooms."""
         # Prepare the message with initial values
-        flattened_rooms = list(set(self._flatten_rooms(self.rooms)))
-        message = {
-            "type": "event",  # Default type, might be overridden by binary or json
-            "event": event,
-            "args": [],
-            "rooms": flattened_rooms,
-            "namespace": self.namespace,
-        }
+        try:
+            if kwargs['namespace']:
+                namespaces = self._flatten_namespaces(kwargs['namespace'])
+        except:
+            namespaces = self.namespace
+            print(namespaces)
+        try:
+            if kwargs['room']:
+                rooms = kwargs['room']
+        except:
+            rooms = list(set(self._flatten_rooms(self.rooms)))
 
-        # Process each argument
-        for arg in args:
-            if isinstance(arg, (bytes, bytearray)):
-                # Handle binary data
-                message["type"] = "binary_event"
-                encoded_arg = base64.b64encode(arg).decode("utf-8")
-                message["args"].append({"_is_binary": True, "data": encoded_arg})
-            elif isinstance(arg, dict):
-                # Handle JSON data
-                message["args"].append(json.dumps(arg))  # Serialize JSON data
+        for __namespace in namespaces:
+            message = {
+                "type": "event",  # Default type, might be overridden by binary or json
+                "event": event,
+                "args": [],
+                "rooms": rooms,
+                "namespace": __namespace
+            }
+
+            # Process each argument
+            for arg in args:
+                if isinstance(arg, (bytes, bytearray)):
+                    # Handle binary data
+                    message["type"] = "binary_event"
+                    encoded_arg = base64.b64encode(arg).decode("utf-8")
+                    message["args"].append({"_is_binary": True, "data": encoded_arg})
+                elif isinstance(arg, dict):
+                    # Handle JSON data
+                    message["args"].append(json.dumps(arg))  # Serialize JSON data
+                else:
+                    # Handle text or other types as is
+                    message["args"].append(arg)
+
+            # Determine the channel based on whether specific rooms are targeted
+            channel = f"{self.key}#{__namespace}#"
+            if rooms:
+                for room in rooms:
+                    room_channel = f"{channel}{room}#"
+                    self.redis_client.publish(room_channel, json.dumps(message))
             else:
-                # Handle text or other types as is
-                message["args"].append(arg)
+                self.redis_client.publish(channel, json.dumps(message))
 
-        # Determine the channel based on whether specific rooms are targeted
-        channel = f"{self.key}#{self.namespace}#"
-        if self.rooms:
-            for room in flattened_rooms:
-                room_channel = f"{channel}{room}#"
-                print(room_channel)
-                self.redis_client.publish(room_channel, json.dumps(message))
-        else:
-            print(channel)
-            self.redis_client.publish(channel, json.dumps(message))
-
-        # Clear the rooms after emitting the message
-        self.rooms.clear()
+            # Clear the rooms after emitting the message
+            self.rooms.clear()
 
     def _create_client(self):
         """Create a Redis client."""
-        return redis.StrictRedis(host=self.host, port=self.port, password=self.password)
+        return redis.StrictRedis(host = self.host, port = self.port, password = self.password)
 
     def _flatten_rooms(self, room_list):
         """Flatten a list of rooms."""
@@ -81,6 +106,10 @@ class Emitter:
             else:
                 flattened_rooms.append(item)
         return flattened_rooms
+
+    def _flatten_namespaces(self, namespaces):
+        """Makes given string list to namespaces list"""
+        return ["/"+np for np in namespaces]
 
     def _has_bin(self, *args):
         """Check if any argument is binary data."""
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def main():
     emitter = Emitter(
         host="localhost",
         port=6379,
-        namespace="/QB_space",
+        namespace=["QB_space"],
         key="socket.io",
     )
 
@@ -27,6 +27,61 @@ def main():
     )
     emitter.in_room("room1").emit("my_event", {"key": "value", "data": [1, 2, 3]})
     emitter.emit("broadcast_event", {"key": "value", "data": [2, 3, 4]})
+    #----------------------------------------------------------------------------------------#
+    #                                                                                        #
+    # Test for emitting messages to multiple namespaces and rooms according to the parameters#
+    #                                                                                        #
+    #----------------------------------------------------------------------------------------#
+
+    # Creates Emitter connected to default namespace '/'
+    emitter1 = Emitter(
+        host = "localhost",
+        port = 6379,
+        key = "socket.io",
+    )
+
+    # creates Emitter connected with one namespace
+    emitter1 = Emitter(
+        host = "localhost",
+        port = 6379,
+        namespace = ["namespace1"],
+        key = "socket.io",
+    )
+
+    # creates Emitter connected with two namespaces
+    # we can provide as many namespaces
+    emitter2 = Emitter(
+        host = "localhost",
+        port = 6379,
+        namespace = ["namespace1", "namespace2"],
+        key = "socket.io",
+    )
+
+    # Adding another namespace to existing Emitter
+    emitter2.of_namespace('namespace3')
+
+    # Removing a namespace from existing Emitter
+    emitter2.off_namespace('namespace2')
+
+    # Sends message to all namespaces connected
+    emitter.emit('message', 'This message is send to all rooms')
+
+    # Sends message to a specified namespace
+    emitter.emit('message', 'Message to Orange', namespace = ['namespace1'])
+
+    # Sends message to a list of specified namespaces
+    emitter.emit('message', 'Message to Orange', namespace = ['namespace1', 'namespace2'])
+
+    # Sends message to a specified rooms in all connected namespaces
+    emitter.emit('message', 'Message to Orange', room = ['room1'])
+
+    # Sends message to a specified rooms in one or more specified namespaces
+    emitter.emit(
+        'message',
+        'Message to Orange',
+        namespace = ['namespace1', 'namespace2'],
+        room = ['room1']
+    )
 
 
 if __name__ == "__main__":
