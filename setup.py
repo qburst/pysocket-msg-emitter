afd79f5bac10e3df8763619768b3cd7811d49dd0
Ankit Mordhwaj <mordhwaj.ankit@gmail.com>
2024-02-27 12:49:10 +0530

    updated wheel

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     author="Ankit Mordhwaj, Soyal Sunny, Ajeesh Anil",
     description="A package for emitting messages via Kafka or Redis in a socket.io-like manner",
-    long_description=open("readme.md").read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/qburst/pysocket-msg-emitter",
     packages=find_packages(),
