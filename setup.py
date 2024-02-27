64fb738998a763bc613b5496b2f53753b9ec23d6
soyal <soyalsunny2000@gmail.com>
2024-02-27 11:22:28 +0530

    change in setup so that both redis and kafka will not be installed

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ setup(
     name="pysocket-msg-emitter",
     version="0.1",
     author="Ankit Mordhwaj, Soyal Sunny, Ajeesh Anil",
-    # author_email='',
     description="A package for emitting messages via Kafka or Redis in a socket.io-like manner",
     long_description=open("readme.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/qburst/pysocket-msg-emitter",
     packages=find_packages(),
     install_requires=[
-        "redis>=3.5.3",
-        "kafka-python>=2.0.2",
+        # Put here any core dependencies that are essential for your package to work
     ],
+    extras_require={
+        "kafka": ["kafka-python>=2.0.2"],
+        "redis": ["redis>=3.5.3"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
